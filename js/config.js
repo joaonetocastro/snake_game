b17969469a695b26afb31304e2468f9a91fd4b5d
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-04 11:04:21 -0300

    responsiviness

--- js/config.js
+++ js/config.js
@@ -1,10 +1,15 @@
-const BLOCK_SIZE = 20;
+const referenceSize = (document.documentElement.clientWidth > document.documentElement.clientHeight ? 
+                      document.documentElement.clientHeight : document.documentElement.clientWidth) - 50;
+                      
+const BLOCKSPERLINE = 20;
+
+const BLOCK_SIZE = Math.floor(referenceSize/BLOCKSPERLINE);
 const FPS = 5;
 const VELOCITY = 1;
 const BLOCKS_PER_ROW = 10;
 const BLOCKS_PER_COLUMN = 10;
-const CANVAS_SIZE = BLOCK_SIZE*BLOCKS_PER_ROW;
-
+const CANVAS_SIZE = BLOCK_SIZE*BLOCKSPERLINE;
+console.log(CANVAS_SIZE);
 const canvas = document.getElementById("canvas"); 
 var context = canvas.getContext("2d"); 
 
